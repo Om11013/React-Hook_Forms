b5deeb823dab1ff48e72726882d36fedf3b071f6
Om <poonjaniom11@gmail.com>
2025-01-21 19:07:44 +0530

    commit

--- src/Form.tsx
+++ src/Form.tsx
@@ -1,4 +1,4 @@
-import { useForm } from 'react-hook-form'
+import { useFieldArray, useForm } from 'react-hook-form'
 import { DevTool } from '@hookform/devtools'
 
 const Form = () => {
@@ -10,20 +10,29 @@ const Form = () => {
       fullname: {
         firstname: "",
         lastname: ""
-      }
+      },
+      phnNumbers: [{contact:""}]
     }
   })
   const { register, control, handleSubmit, formState } = form
   const { errors } = formState
+
+  const { fields,append, remove } = useFieldArray({
+    name: 'phnNumbers',
+    control
+  })
   
-    type FormValues = {
-        username: string,
-        email: string,
-        number: number,
-        fullname: {
-          firstname: string,
-          lastname: string
-        }
+  type FormValues = {
+    username: string,
+    email: string,
+    number: number,
+    fullname: {
+      firstname: string,
+      lastname: string
+    }
+    phnNumbers: { 
+      contact: string
+    }[]
     }
     const submitMethod = (data:FormValues) => {
         console.log(data)
@@ -59,6 +68,28 @@ const Form = () => {
               <div>
                 <label htmlFor="lastname">lastname</label>
                 <input type='text' id='lastname' {...register("fullname.lastname")} />
+              </div>
+              <div>
+                <label htmlFor="listofno">List of Contacts</label>
+                <div>
+                  {
+                    fields.map((field,index)=>{
+                      return (
+                        <div className='form-control'key={field.id}>
+                          <input type="text" {...register(`phnNumbers.${index}.contact` as const)} />
+                          {
+                            index > 0 && (
+                              <button type='button' onClick={()=>{remove(index)}}>Remove number</button>
+
+                            )
+                          }
+                        </div>
+                      )
+                      
+                    })
+                  }
+                  <button type='button' onClick={()=>{append({contact: ""})}}>Add number</button>
+                </div>
               </div>
               <button >Submit</button>
           </form>
