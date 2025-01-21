725564dda79f5cf36f426608051b0dd4f35c9da9
Om <poonjaniom11@gmail.com>
2025-01-21 16:58:37 +0530

    commit

--- src/Form.tsx
+++ src/Form.tsx
@@ -3,8 +3,10 @@ import { DevTool } from '@hookform/devtools'
 
 const Form = () => {
 
-    const form = useForm<FormValues>()
-    const { register, control,handleSubmit } = form
+  const form = useForm<FormValues>()
+  const { register, control, handleSubmit, formState } = form
+  const { errors } = formState
+  
     type FormValues = {
         username: string,
         email: string,
@@ -18,15 +20,18 @@ const Form = () => {
           <form onSubmit={handleSubmit(submitMethod)}>
               <div>  
                 <label htmlFor="username">UserName</label>
-                <input type='text' id='username' {...register("username",{required:"Username is required"})} />
+                <input type='text' id='username' {...register("username", { required: "Username is required" })} />
+                <p>{errors.username?.message}</p>
               </div>
               <div>    
                 <label htmlFor="email">Email</label>
-                <input type='email' id='email' {...register("email", {pattern: {value:/.*?@?[^@]*\.+.*/,message:"Invalid format"}})} />
+                <input type='text' id='email' {...register("email", {pattern: {value:/.*?@?[^@]*\.+.*/,message:"Invalid format"}})} />
+                <p>{errors.email?.message}</p>
               </div>
               <div>  
                 <label htmlFor="number">Number</label>
                 <input type='tel' id='number' {...register("number")} />
+                <p>{errors.number?.message}</p>
               </div>
               <button >Submit</button>
           </form>
