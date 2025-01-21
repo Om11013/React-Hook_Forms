ce63ecad98a091a15622fda2ea58d27ff273ebe8
Om <poonjaniom11@gmail.com>
2025-01-21 18:17:46 +0530

    commit

--- src/Form.tsx
+++ src/Form.tsx
@@ -3,14 +3,27 @@ import { DevTool } from '@hookform/devtools'
 
 const Form = () => {
 
-  const form = useForm<FormValues>()
+  const form = useForm<FormValues>({
+    defaultValues: {
+      username: "Batman",
+      email: "",
+      fullname: {
+        firstname: "",
+        lastname: ""
+      }
+    }
+  })
   const { register, control, handleSubmit, formState } = form
   const { errors } = formState
   
     type FormValues = {
         username: string,
         email: string,
-        number: number
+        number: number,
+        fullname: {
+          firstname: string,
+          lastname: string
+        }
     }
     const submitMethod = (data:FormValues) => {
         console.log(data)
@@ -25,13 +38,27 @@ const Form = () => {
               </div>
               <div>    
                 <label htmlFor="email">Email</label>
-                <input type='text' id='email' {...register("email", {pattern: {value:/.*?@?[^@]*\.+.*/,message:"Invalid format"}})} />
+              <input type='text' id='email' {...register("email", {
+                pattern: { value: /.*?@?[^@]*\.+.*/, message: "Invalid format" },
+                validate: (field) => {
+                  return (
+                    field!="admin@gmail.com"||"Enter a different email"
+                  )
+                }})} />
                 <p>{errors.email?.message}</p>
               </div>
               <div>  
                 <label htmlFor="number">Number</label>
                 <input type='tel' id='number' {...register("number")} />
                 <p>{errors.number?.message}</p>
+              </div>
+              <div>
+                <label htmlFor="firstname">firstname</label>
+                <input type='text' id='firstname' {...register("fullname.firstname")} />
+              </div>
+              <div>
+                <label htmlFor="lastname">lastname</label>
+                <input type='text' id='lastname' {...register("fullname.lastname")} />
               </div>
               <button >Submit</button>
           </form>
